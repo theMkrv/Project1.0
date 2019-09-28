3c179b7f2f7c8471967859743b4a9f3b5a49efe8
theMkrv <themkrv@gmail.com>
2019-09-28 13:03:10 +0700

    dead 40 points

--- Voyadjer1.py
+++ Voyadjer1.py
@@ -7,7 +7,6 @@ range_km = total_range * 1.609
 a_e = range_km / 150000000
 radiowave_speed = 299792458 / 1000 * 3600
 delay = range_km / radiowave_speed
-delay = math.floor(delay)
 print(a_e,"А.Е.")
 print(range_km,"Км")
 print(total_range,"Миль")
